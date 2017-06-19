5304e38a776512c901bc2bd2d27c95d0aea0aa5a
dineshmakkar079 <dineshmakkar079@gmail.com>
2017-06-19 12:02:28 +0530

    Extension works at document end and limited line length is typed in file opened in IDE

--- codex-server.js
+++ codex-server.js
@@ -6,26 +6,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 var fs = require('fs');
 var exec = require('child_process').exec;
 app.post('/',(req,res)=>{
+	console.log('post requset recieved')
 	var probStatement = req.body.statement;
 	var probTitle = req.body.title;
 	var probUrl = req.body.url;
 	var host = req.body.host;
 
 	probTitle = probTitle.split(' ').join('_');
-	//if(probTitle.length){probTitle}
-	//console.log(probStatement);
+
+	// To display limited line length in IDE.
 	l = probStatement.length;
 	newProbStatement = '';
-	var maxLength = 95, start = 0;
+	var maxLength = 95, start = 0,end = maxLength;
 	while(start < l){
-		newProbStatement += probStatement.substr(start, maxLength) + '\n';
-		start += maxLength;
+		for(var i=end;i<l && probStatement[i]!=' ';i++);
+		end = i+1;
+		newProbStatement = newProbStatement + probStatement.substring(start,end) + '\n';
+		start = end ;
+		end = start + maxLength;
 	}
 	probStatement = newProbStatement;
+
+	// Make new file only if file doesn't exist already (i.e. if opened earlier) 
+	if(fs.readdirSync('/home/makkar/Documents/Coding/'+ host + '/').indexOf(probTitle + '.cpp') == -1)
+		fs.appendFileSync('/home/makkar/Documents/Coding/'+ host + '/' + probTitle + '.cpp' , '/*\nTime : ' +  (new Date()).toString()  +'\nURL : ' + probUrl + '\n' + probStatement +'\n-------------------------------------------------------\n*/\n')
 	
-	fs.appendFileSync('/home/makkar/Documents/Coding/'+ host + '/' + probTitle + '.cpp' , '/*\nURL : ' + probUrl + '\n' + probStatement +'\n-------------------------------------------------------\n*/\n')
-	
-	//console.log(probStatement)
 	exec("/home/makkar/Downloads/VSCode-linux-x64/code /home/makkar/Documents/Coding/" + host + "/"+probTitle+".cpp &", ()=>{
 	 	console.log('launched VS Code.')
 		res.send("Done");
